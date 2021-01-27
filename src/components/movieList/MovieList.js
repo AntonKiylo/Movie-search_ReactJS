ddd96cefe66e63e452ff57e50c8fd2d7d481397d
anton <kiyloanton@gmail.com>
2021-01-27 11:50:54 +0300

    stylized MovieList

--- src/components/movieList/MovieList.js
+++ src/components/movieList/MovieList.js
@@ -1,13 +1,17 @@
 import './MovieList.css';
+import heartIcon from '../../assets/heart_icon.png';
 
 const MovieList = ({ movies }) => {
   return (
     <div className='movie-list'>
       {movies.map(movie => {
         return (
-          <div className='poster'>
-            <img src={movie.Poster} key={movie.imdbID} alt='Poster' />
-            <div className='poster-footer'>footer</div>
+          <div className='poster' key={movie.imdbID}>
+            <img src={movie.Poster} alt='Poster' />
+            <div className='poster-footer'>
+              <p>Add to Favorites</p>
+              <img src={heartIcon} alt='heart-icon' />
+            </div>
           </div>
         );
       })}
